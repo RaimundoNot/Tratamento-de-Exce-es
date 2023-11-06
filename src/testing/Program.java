0dc0da60c48c4d8b7658ace60b24961b1ea8a3ac
RaimundoNot <raimundomotadeol@gmail.com>
2023-11-06 15:21:50 -0400

    Entendendo StackTracing

--- src/testing/Program.java
+++ src/testing/Program.java
@@ -6,26 +6,32 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
+		method1();
 
-		
+		System.out.println("End of Program");
+	}
+	public static void method1() {
+		System.out.println("*** METHOD1 START! ***");
+		method2();
+		System.out.println("*** METHOD1 END! ***");
+	}
+	
+	public static void method2() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("*** METHOD2 START! ***");
 		try {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
 			System.out.println(vect[position]);
-		}
-		catch (ArrayIndexOutOfBoundsException e) {
+		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Ivalid postion!");
-		}
-		catch(InputMismatchException e) {
+			e.printStackTrace();
+			sc.next();
+		} catch (InputMismatchException e) {
 			System.out.println("Input Error!");
 		}
-		
-		System.out.println("End of Program");
-		
+
+		System.out.println("*** METHOD2 END! ***");
 		sc.close();
-
 	}
-
 }
