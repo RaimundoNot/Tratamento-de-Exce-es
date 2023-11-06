59b7f11b8bd7340905e9c06c160538894090d68d
RaimundoNot <raimundomotadeol@gmail.com>
2023-11-06 15:05:21 -0400

    Entendendo Try-Catch

---
+++ src/testing/Program.java
@@ -0,0 +1,31 @@
+package testing;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Ivalid postion!");
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Input Error!");
+		}
+		
+		System.out.println("End of Program");
+		
+		sc.close();
+
+	}
+
+}
