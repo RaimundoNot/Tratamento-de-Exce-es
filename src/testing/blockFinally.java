668eb7da8acd1db4c5cb0bf3383692abcf818708
RaimundoNot <raimundomotadeol@gmail.com>
2023-11-07 14:52:30 -0400

    Entendo o Bloco Finally

---
+++ src/testing/blockFinally.java
@@ -0,0 +1,25 @@
+package testing;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class blockFinally {
+	public static void main(String[] args) {
+		File file = new File("C:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (IOException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		} finally {
+			if (sc != null) {
+				sc.close();
+			}
+			System.out.println("Block Finally Executed!");
+		}
+	}
+}
