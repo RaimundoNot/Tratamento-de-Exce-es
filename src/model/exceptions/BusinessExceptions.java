5c8a8dd3e50e4305eab205b3ff5008fbea267c87
RaimundoNot <raimundomotadeol@gmail.com>
2023-11-09 16:42:51 -0400

    Exercicio de Fixação

---
+++ src/model/exceptions/BusinessExceptions.java
@@ -0,0 +1,9 @@
+package model.exceptions;
+
+public class BusinessExceptions extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+
+	public BusinessExceptions (String msg) {
+		super(msg);
+	}
+}
